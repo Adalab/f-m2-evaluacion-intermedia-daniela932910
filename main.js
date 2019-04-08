780a5a978c0a8e59edf7d9dc05b18d3dbe000bad
daniela932910 <daniela932910@gmail.com>
2019-04-08 17:26:01 +0200

    add steps for exam

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 'use strict'; 
+
+
+// crear un juego en el cual se tiene que generar un numero y el usuario tiene que adivinar un numero, a manera de que va introduciendo el numero se le va indicando mediante un mensaje si esta cerca o si ha acertado, a la vez se le indica cuantos intentos lleva
+
+//primero es crear el html:
+   // con un h1 con el texto adivina el numero;     un input type text                                 un boton que diga prueba                           un p que diga escribe un numero y dale a la prueba   un p para el numero de intentos
+
+//segundo es en css poner un flex al body para que se ponga todo en columna centrado
+
+//tercero en el fichero de js:
+
+    // recojer el elemento input
+
+    // recojer el boton prueba
+
+    // recojer el elemento feedback
+
+    //crear una funccion para generar el numero aleatorio
+
+    // crear el listener para el boton prueba
+
+    // crear la funncion con un if si en el input se escribe un numero que indique si esta mayor o menor que el random number que indique en el feedback si es mayor o menor que el random number
+
+    // crear una funccion para que indique los intnentos y indicarlos en el inner.html del p para intentos
+
+//poner css segun ej ejemplo
+
+    
